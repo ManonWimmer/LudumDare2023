a4e06b8183faec871c6eda721e69d2398f45de9f
LucLenne <113776725+LucLenne@users.noreply.github.com>
2023-10-01 17:16:01 +0200

    musique+reglage code

--- Assets/Scripts/Luc/EnigmeBinaire.cs
+++ Assets/Scripts/Luc/EnigmeBinaire.cs
@@ -11,17 +11,49 @@ public class EnigmeBinaire : MonoBehaviour
     private string codeBinaire = "1001";
     private string codePlayer="";
     [SerializeField] TextMeshProUGUI textMeshPro;
-
+    [SerializeField] TextMeshProUGUI textMeshProPW;
 
     public void CodePLayer(string chiffre)
     {
-        if (codePlayer.Length >= 4)
-        {
-            codePlayer = "";
-        }
         codePlayer += chiffre;
-        string texte = textMeshPro.text;
-        Debug.Log("TextMeshPro Text: " + texte);
-        Debug.Log(codePlayer);
+        textMeshPro.text = codePlayer;
+        if (codePlayer.Length == 3)
+        {
+            if (codePlayer == "000")
+            {
+                textMeshProPW.text += "0";
+
+            }
+            else if(codePlayer == "001")
+            {
+                textMeshProPW.text += "1";
+            }
+            else if (codePlayer == "010")
+            {
+                textMeshProPW.text += "2";
+            }
+            else if (codePlayer == "011")
+            {
+                textMeshProPW.text += "3";
+            }
+            else if (codePlayer == "100")
+            {
+                textMeshProPW.text += "4";
+            }
+            else if (codePlayer == "101")
+            {
+                textMeshProPW.text += "5";
+            }
+            else if (codePlayer == "110")
+            {
+                textMeshProPW.text += "6";
+            }
+            else if (codePlayer == "111")
+            {
+                textMeshProPW.text += "";
+            }
+            codePlayer = "";
+            textMeshPro.text = codePlayer;
+        }
     }
 }
