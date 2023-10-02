7c45f06b8c3f8c15e184aebf8f45ae4b09a7d488
LucLenne <113776725+LucLenne@users.noreply.github.com>
2023-10-02 19:34:17 +0200

    scene Couloir

---
+++ Assets/Scripts/Luc/loadScene2.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class loadScene2 : MonoBehaviour
+{
+    bool inCollider = false;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        inCollider = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        inCollider = false;
+    }   
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && inCollider)
+        {
+            SceneManager.LoadScene("Luc");
+        }
+    }
+}
