4862503fe2d211b93ff0174aecab08fb8ddbabe4
LucLenne <113776725+LucLenne@users.noreply.github.com>
2023-10-02 16:47:53 +0200

    reglage son

--- Assets/Scripts/Luc/Bone0.cs
+++ Assets/Scripts/Luc/Bone0.cs
@@ -6,14 +6,17 @@ public class Bone0 : MonoBehaviour
 {
     [SerializeField] EnigmeBinaire enigmeBinaire;
     [SerializeField] int rotationX = 150;
+    [SerializeField] Sound Sound;
     public void activationLevier(GameObject bone)
     {
         bone.transform.Rotate(rotationX, 0, 0, Space.Self);
+        Sound.playSound();
         StartCoroutine(MyCoroutine());
         IEnumerator MyCoroutine()
         {
             yield return new WaitForSeconds(1.0f);
             bone.transform.Rotate(-rotationX, 0, 0, Space.Self);
+            Sound.playSound();
         }
     }
 }
--- Assets/Scripts/Luc/Bone1.cs
+++ Assets/Scripts/Luc/Bone1.cs
@@ -6,14 +6,17 @@ public class Bone1 : MonoBehaviour
 {
     [SerializeField] int rotationX = 150;
     [SerializeField] EnigmeBinaire enigmeBinaire;
+    [SerializeField] Sound Sound;
     public void activationLevier(GameObject bone)
     {
         bone.transform.Rotate(rotationX, 0, 0, Space.Self);
+        Sound.playSound();
         StartCoroutine(MyCoroutine());
         IEnumerator MyCoroutine()
         {
             yield return new WaitForSeconds(1.0f);
             bone.transform.Rotate(-rotationX, 0, 0, Space.Self);
+            Sound.playSound();
         }
     }
 }
